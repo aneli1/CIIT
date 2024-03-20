c2595ee4a0146b31bde97f2958eee42dc70081a3
FridaMartinezLorenzo <117133767+FridaMartinezLorenzo@users.noreply.github.com>
2024-03-20 13:32:52 -0600

    quitando el src extra

--- src/app/models/Rol.ts
+++
@@ -1,9 +0,0 @@
-export class Rol{
-    id_rol: number;
-    nombre_rol : string;
-
-    constructor() {
-        this.id_rol = 1,
-        this.nombre_rol = "Administrador"
-    }
-}
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  API_URI: "http://localhost:3000/api"
-};
